bb68e6fc2545f53a4c7f6ca0f40539b626450c49
morit <morit@Moritz.fritz.box>
2025-03-11 08:50:34 +0100

    SQL speichern funktioniert

--- Fallstudie1/src/main/java/likeherotozero/Co2ListeController.java
+++ Fallstudie1/src/main/java/likeherotozero/Co2ListeController.java
@@ -1,6 +1,6 @@
 package likeherotozero;
-
 import java.io.Serializable;
+import java.util.List;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
@@ -10,22 +10,50 @@ import jakarta.persistence.*;
 @ViewScoped
 public class Co2ListeController implements Serializable
 {
-	@Inject 
-	private Co2Liste co2liste;
-	
+    private static final long serialVersionUID = 1L;
+    
+    @Inject 
+    private Co2Liste co2liste;
+    
+    private List<Co2Emission> emissionList;
+    
+    // Die richtige Getter-Methode für EmissionList
+    public List<Co2Emission> getEmissionList() {
+        if (emissionList == null) {
+            emissionList = co2liste.getListe();
+        }
+        return emissionList;
+    }
+    
     public String startEdit() {
         return "login";
     }
     
     public String stopEdit() {
-    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("emissionPersistenceUnit");
-    	
-    	EntityManager em = emf.createEntityManager();
-        EntityTransaction t = em.getTransaction();
-        t.begin();
-        for (Co2Emission a : co2liste.getListe())
-          em.merge(a);
-        t.commit();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("emissionPersistenceUnit");
+        EntityManager em = emf.createEntityManager();
+        
+        try {
+            EntityTransaction t = em.getTransaction();
+            t.begin();
+            
+            for (Co2Emission a : getEmissionList()) {
+                System.out.println("Speichere: " + a.getLand() + ", Wert: " + a.getCo2Wert());
+                em.merge(a);
+            }
+            
+            t.commit();
+            System.out.println("Speichern abgeschlossen");
+        } catch (Exception e) {
+            System.err.println("Fehler beim Speichern: " + e.getMessage());
+            e.printStackTrace();
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+        } finally {
+            em.close();
+        }
+        
         return "index";
     }
 }
